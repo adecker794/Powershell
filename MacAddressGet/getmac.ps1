741050df2ce603e03347fadba6f9bc65f5a435e7
adecker794 <41381687+adecker794@users.noreply.github.com>
2019-02-02 23:00:45 -0600

    Add files via upload

---
+++ MacAddressGet/getmac.ps1
@@ -0,0 +1,12 @@
+﻿#Gets the list of computers
+$computers = Get-Content c:\temp\scripts\getmac\computers.txt
+#$computers = 'computer'
+
+#Foreach's each computer in the list and then get its mac and adds it to a csv file
+foreach($computer in $computers)
+{ 
+    Write-Host "Starting $computer"
+    get-netadapter –CimSession $computer| select SystemName, Name, MacAddress  | Export-CSV -path c:\temp\scripts\getmac\macs.csv -append
+    Write-Host "Ending $computer"
+
+} 
---
+++ MacAddressGet/readme.txt
@@ -0,0 +1,18 @@
+What these sets of scripts do:
+It goes to each machine in a list and pulls the Mac Adresses of all network cards.
+
+
+Where to place scripts:
+c:\temp\scripts\getmac
+
+
+What to change:
+c:\temp\scripts\getmac\computers.txt - add in your list of computers
+
+Output:
+c:\temp\scripts\getmac\macs.csv - it is a list of all macs obtained from the list of computers
+
+
+How to run:
+Open in ISE and run(after defining computers.txt)
+Or just run from CLI
